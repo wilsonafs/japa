5f36e519efd14bfbc2d854a5fba77298f1d4c3a4
Gianpaulo M. Soares <jam_pow@hotmail.com>
2019-01-10 11:03:49 -0200

    feat: campo para pode ser modificado via parâmetro de url

--- count.js
+++ count.js
@@ -1,23 +1,34 @@
+const getParam = name => {
+  const url = new URL(location.href)
+  return url.searchParams.get(name)
+}
+
 const second = 1000,
       minute = second * 60,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Jan 10, 2019 12:00:00').getTime(),
-    x = setInterval(function() {
+const countDown = new Date('Jan 10, 2019 12:00:00').getTime()
 
-      let now = new Date().getTime(),
-          distance = countDown - now;
+setInterval(function() {
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-      
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'Hora do Japa!;
-      //}
+  const now = new Date().getTime(),
+    distance = countDown - now;
 
-    }, second)
+  document.getElementById('days').innerText = Math.floor(distance / (day)),
+  document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+  document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+  document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+  //do something later when date is reached
+  //if (distance < 0) {
+  //  clearInterval(x);
+  //  'Hora do Japa!;
+  //}
+
+}, second)
+
+const paraEl = document.getElementById('para')
+const para = getParam('para')
+if (para) paraEl.innerText = para
+
